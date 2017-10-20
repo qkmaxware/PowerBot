7d3d0396bf7d7fbd5c1234423fb4720202bd729d
Colin Halseth <qkmaxware@gmail.com>
2017-10-19 18:41:18 -0700

    Modified debug func

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using DiscordBot.Bot;
 using SimpleWebServer;
 using FlatFileDatabase;
 
@@ -41,32 +42,25 @@ namespace DiscordBot {
         /// </summary>
         static void Debug() {
             //Create default JSON
-            BotConfig conf = new BotConfig();
-            BotConfig.Serialize(configRef, conf);
+            BotConfig config = BotConfig.Deserialize("configRef");
 
+            //Create the Database
             FlatFileDatabase.Database db = Database.Create("db.json");
-            FlatFileDatabase.QueryCompiler compiler = new QueryCompiler();
-            Query query = Query.Compile("CREATE TABLE tasks ( uid, tid, description )");
-            Query query2 = Query.Compile("DROP TABLE tasks");
-            Query query3 = Query.Compile("INSERT INTO tasks (uid,tid,description) VALUES 0,1,'my value'");
-            Query query4 = Query.Compile("INSERT INTO tasks (uid,tid,description) VALUES 1,2,'my value 2'");
-            Query query5 = Query.Compile("UPDATE tasks SET uid = 1 WHERE uid = 0 AND tid = 1");
-            Query query6 = Query.Compile("SELECT * FROM tasks WHERE uid = 1");
-            Query query7 = Query.Compile("DELETE FROM tasks WHERE uid = 1 AND tid = 1");
-            db.Query(query);
-            db.Query(query3);
-            db.Query(query4);
-            db.Query(query5);
-            Console.WriteLine(db.Query(query6));
-            //db.Query(query7);
-            //Console.WriteLine(db.Query(query6));
-            //db.Query(query2);
 
+            //Create the bot
+            Bot.Bot bot = new Bot.Bot(config);
+
+            //Initialize the webserver
             SimpleWebServer.WebServer web = new SimpleWebServer.WebServer();
             SimpleWebServer.WebPage index = new SimpleWebServer.WebPage("Testing 1,2,3");
             web.AddPage("index", index);
 
             web.Start();
+
+            //Listen asynchronously
+            Task t = bot.Connect(); //.GetAwaiter().GetResult();
+
+            //Wait until told otherwise
             Console.WriteLine("A Simple Web Server. Press esc to stop.");
             while (true) {
                 if (Console.ReadKey().Key == ConsoleKey.Escape)
@@ -80,14 +74,7 @@ namespace DiscordBot {
         /// Run the bot
         /// </summary>
         static void Run() {
-            //Load the configs
-            BotConfig config = BotConfig.Deserialize(configRef);
 
-            //Create the bot
-            Bot bot = new Bot(config);
-
-            //Listen asynchronously
-            bot.Connect().GetAwaiter().GetResult();
         }
     }
 }
